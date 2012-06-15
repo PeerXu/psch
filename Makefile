c45cf0b219fb94077862ca5b2f79e865607a7a23
Peer Xu <pppeerxu@gmail.com>
2012-06-15 10:53:47 +0800

    create a simple REPL

--- Makefile
+++ Makefile
@@ -2,7 +2,10 @@ BIN=bin
 SRC=src
 
 build:
-	gcc -o $(BIN)/hello $(SRC)/main.c
+	gcc -g -o $(BIN)/psch $(SRC)/main.c
+
+bt:
+	gcc -g -o $(BIN)/test $(SRC)/test.c
 
 clean:
 	rm $(BIN)/*
--- src/main.c
+++ src/main.c
@@ -1,6 +1,72 @@
 #include <stdio.h>
 
+#define BUFFSIZE 1024
+
+char *version = "0.0.1";
+
+void eval_loop();
+int read_expr(char *buff);
+int syntax_check(char *expr);
+
+void eval_loop() {
+  char buff[BUFFSIZE];
+  printf("*>");
+  //while(scanf("%s", buff)) {
+  while(read_expr(buff)) {
+    if (!syntax_check(buff)) {
+      printf("[ERR]: syntax error\n");
+    }
+    else {
+      printf("%s\n", buff);
+    }
+    printf("*>");
+  }
+}
+
+int read_expr(char *buff) {
+  int size = 0;
+  char *buf = buff;
+  char c;
+
+  for (c = getchar(); c != '\n'; c = getchar()) {
+    buf[size++] = c;
+  }
+  buf[size] = '\0';
+  return size;
+}
+
+int syntax_check(char *expr) {
+  int expr_count=0;
+  int string_count=0;
+  char *p = expr;
+
+  while (*p != '\0') {
+    if (string_count) {
+      if (*p == '"') {
+	--string_count;
+      }
+    }
+    else if (*p == '"') {
+      ++string_count;
+    }
+    else if (*p == '(') {
+      ++expr_count;
+    }
+    else if (*p == ')') {
+      --expr_count;
+    }
+    ++p;
+  }
+
+  if (expr_count || string_count) {
+    return 0;
+  }
+  return 1;
+}
+
 int main(void) {
-  printf("hello, world\n");
+  printf("pscsh -- scheme interpreter\n");
+  printf("version: %s\n", version);
+  eval_loop();
   return 0;
 }
